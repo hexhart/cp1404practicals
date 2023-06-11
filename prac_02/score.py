1760c42019c03633d4e05fcb855b7a6833434f62
Hexon Hartley <hexonhartley.jimenez@my.jcu.edu.au>
2023-06-11 10:57:01 +1000

    Add main function to get user score and new function to generate random number and print result

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,17 +1,29 @@
 """
 CP1404/CP5632 - Practical
-Broken program to determine score status
+Program that determines score status
 By Hexon Hartley Jimenez
 """
+import random
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-elif score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    score = float(input("Enter score: "))
+    determine_score(score)
+
+    random_score = random.randint(0, 100)
+    print(f"Random Score: {random_score}")
+    determine_score(random_score)
+
+
+def determine_score(score):
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+
+main()
