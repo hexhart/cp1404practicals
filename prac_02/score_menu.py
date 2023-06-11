eb2965585bc82382e4047155401d6f232188188e
Hexon Hartley <hexonhartley.jimenez@my.jcu.edu.au>
2023-06-11 13:19:24 +1000

    Add several functions for each corresponding program menu options

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,29 +1,68 @@
 """
 CP1404/CP5632 - Practical 2
-Updated Program that determines score status
+Score Menu Program
 By Hexon Hartley Jimenez
 """
 
+MENU = """
+(G)et a valid score
+(P)rint result
+(S)how stars
+(Q)uit
+"""
+
+
 def main():
-    display  menu
-    get choice
-    while choice != < quit option >
-        if choice == < first option >
-        < do first task >
+    score = -1  # To initialize invalid score upon selecting P first, when no valid input is entered
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
 
-    else if choice == < second option >
-        < do second task >
-    ...
-    else if choice == < n - th option >
-        < do n - th task >
-    else
-        display invalid input error message
-    display menu
-    get choice
-<do final thing, if needed >
+        if choice == "G":
+            score = get_valid_score()
+
+        elif choice == "P":
+            print_score_result(score)
+
+        elif choice == "S":
+            print_star(score)
+        else:
+            print("Invalid Choice")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("farewell")
 
 
+def get_valid_score():
+    score = int(input("Enter valid score between 0-100: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = int(input("Enter valid score: "))
+    return score
 
+
+def determine_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def print_score_result(score):
+    score_result = determine_score(score)
+    print(f"Score result: {score_result}")
+
+
+def print_star(score):
+    if score < 0 or score > 100:
+        print("Invalid score, enter a valid number first!")
+    else:
+        star_count = "*" * score
+        print(f"{star_count}")
 
 
 main()
