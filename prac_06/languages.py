306fa00974bca874089930aedaee2fb1f762853a
Hexon Hartley <hexonhartley.jimenez@my.jcu.edu.au>
2023-07-16 12:56:11 +1000

    Add print function to ruby and visual_basic for testing

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -4,3 +4,17 @@ Intermediate Exercise: Programming languages client code file
 Estimated: 30 minutes
 Actual:
 """
+
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    """Main function of the programming language"""
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+    print(ruby)
+    print(visual_basic)
+
+main()
