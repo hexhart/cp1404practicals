63ad11726d5e0bf4de5982c2c6db9546182e2e50
Hexon Hartley <hexonhartley.jimenez@my.jcu.edu.au>
2023-07-16 12:46:48 +1000

    Update ProgrammingLanguage file with class codes

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -4,3 +4,18 @@ Intermediate Exercise: Programming languages class file
 Estimated: 30 minutes
 Actual:
 """
+
+
+class ProgrammingLanguage:
+    """Class representing programming langauge"""
+
+    def __init__(self, name, typing, reflection, year):
+        """Construct of ProgrammingLanguage from the given task details"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """This determines if language is dynamically types or not"""
+        return self.typing == "Dynamic"
