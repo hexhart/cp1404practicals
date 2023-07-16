dd2ed52adfcf23891821d8d224e2dfbb6bfcf67d
Hexon Hartley <hexonhartley.jimenez@my.jcu.edu.au>
2023-07-16 13:19:25 +1000

    Add __str__ and get_age() methods

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -4,7 +4,7 @@ Guitars class file
 Estimated: 30 minutes
 Actual:
 """
-
+CURRENT_YEAR = 2023
 
 class Guitar:
     """Class representing Guitars"""
@@ -13,4 +13,14 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Return string format of the Guitar"""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Returns how old the guitar is in years"""
+        return CURRENT_YEAR - self.year
+
+
 
