aaa5155e88b014436ec7e649e9662d0251a0ec75
Hexon Hartley <hexonhartley.jimenez@my.jcu.edu.au>
2023-07-16 12:34:05 +1000

    Add second prac06 exercise files and header docstrings

---
+++ prac_06/languages.py
@@ -0,0 +1,6 @@
+"""
+CP1404/CP5632 Practical 6 By Hexon Hartley Jimenez
+Intermediate Exercise: Programming languages client code file
+Estimated: 30 minutes
+Actual:
+"""
---
+++ prac_06/programming_language.py
@@ -0,0 +1,6 @@
+"""
+CP1404/CP5632 Practical 6 By Hexon Hartley Jimenez
+Intermediate Exercise: Programming languages class file
+Estimated: 30 minutes
+Actual:
+"""
