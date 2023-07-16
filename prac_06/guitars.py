a7bda31bb55ad8ecf5f33498be462b7b57885195
Hexon Hartley <hexonhartley.jimenez@my.jcu.edu.au>
2023-07-16 13:14:07 +1000

    Add __init__ method for the guitar constructs

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -4,3 +4,13 @@ Guitars class file
 Estimated: 30 minutes
 Actual:
 """
+
+
+class Guitar:
+    """Class representing Guitars"""
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise the Guitar constructs"""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
