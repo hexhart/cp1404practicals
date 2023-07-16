e82a3b1a7230e659e59f06a77946c67d410930dc
Hexon Hartley <hexonhartley.jimenez@my.jcu.edu.au>
2023-07-16 17:15:39 +1000

    Complete guitars client program and update header docstring

---
+++ prac_06/guitars.py
@@ -0,0 +1,34 @@
+"""
+CP1404/CP5632 Practical 6 By Hexon Hartley Jimenez
+Guitar client program
+Estimated: 15 minutes
+Actual: 40 minutes
+"""
+from prac_06.guitar import Guitar
+
+
+def main():
+    """Guitar client program that utilizes the Guitar class"""
+    guitars = []  # create a new list
+
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.")
+        name = input("Name: ")
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    guitars.sort(reverse=True)
+
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+main()
