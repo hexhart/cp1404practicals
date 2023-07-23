f55837ff86f876c0434e349d5ebb5b21bde90414
Hexon Hartley <hexonhartley.jimenez@my.jcu.edu.au>
2023-07-23 16:56:22 +1000

    Import datetime, class and add menu list

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,11 +4,24 @@ Project Management Client Program
 Estimated:
 Actual:
 """
+import datetime
 from prac_07.project import Project
+
+FILENAME = "project.txt"
+MENU = """- (L)oad projects  
+- (S)ave projects  
+- (D)isplay projects  
+- (F)ilter projects by date
+- (A)dd new project  
+- (U)pdate project
+- (Q)uit"""
 
 
 def main():
-    pass
+    """
+    Main function to run the travel tracker program
+    """
+    print(MENU)
 
 
 if __name__ == "__main__":
