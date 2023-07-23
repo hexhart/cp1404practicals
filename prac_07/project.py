7c6a338037ffbdecb0f11c2222c1b667dcb9afe2
Hexon Hartley <hexonhartley.jimenez@my.jcu.edu.au>
2023-07-23 16:47:51 +1000

    Add class definition and constructs for Project class

--- prac_07/project.py
+++ prac_07/project.py
@@ -9,5 +9,14 @@ Actual:
 class Project:
     """Class representing the Project"""
 
-    def __init__(self):
-        pass
+    def __init__(self, name, start_date, priority, cost_estimate, percent_complete):
+        """Constructs of Project from the given task details"""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.percent_complete = percent_complete
+
+    def __str__(self):
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, " \
+               f"estimate: ${self.cost_estimate:.2f}, completion: {self.percent_complete}%"
