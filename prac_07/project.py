a8b0e06ca20dc9be0d7aad375fcffe761a58268d
Hexon Hartley <hexonhartley.jimenez@my.jcu.edu.au>
2023-07-23 13:50:57 +1000

    Add project management exercise files with txt file, header docstring, class and function definitions

---
+++ prac_07/project.py
@@ -0,0 +1,13 @@
+"""
+CP1404/CP5632 Practical 7 By Hexon Hartley Jimenez
+Project Class file
+Estimated:
+Actual:
+"""
+
+
+class Project:
+    """Class representing the Project"""
+
+    def __init__(self):
+        pass
---
+++ prac_07/project_management.py
@@ -0,0 +1,15 @@
+"""
+CP1404/CP5632 Practical 7 By Hexon Hartley Jimenez
+Project Management Client Program
+Estimated:
+Actual:
+"""
+from prac_07.project import Project
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
