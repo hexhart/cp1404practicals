9f6592b384494d73086d0b619d83a436df73df22
Hexon Hartley <hexonhartley.jimenez@my.jcu.edu.au>
2023-07-30 14:10:30 +1000

    Add header docstring, import and new new class definition

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,12 @@
+"""
+CP1404/CP5632 Practical 8 By Hexon Hartley Jimenez
+Modifying Existing GUI Program - box_layout_demo.py
+Estimated Time: 30 minutes
+Actual Time: 25 minutes
+"""
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class DistanceConverterApp(App):
+    pass
