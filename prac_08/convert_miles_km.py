5e50262704475d43941d7ada570d7b2826add915
Hexon Hartley <hexonhartley.jimenez@my.jcu.edu.au>
2023-07-30 17:10:02 +1000

    Add draft handle_increment method and working handle_calculation method

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -10,15 +10,28 @@ from kivy.core.window import Window
 
 __author__ = 'Hexon Hartley Jimenez'
 
+MILES_TO_KM = 1.60934
+
 
 class DistanceConverterApp(App):
     """ DistanceConverterApp is a program that converts miles to kilometres"""
+
     def build(self):
         """ build the Kivy app from the kv file """
         Window.size = (750, 450)
         self.title = "Miles to Kilometres Converter"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_increment(self, update):
+        """Handles the buttons up and down upon button press"""
+
+    pass
+
+    def handle_calculation(self):
+        value = float(self.root.ids.input_distance.text)
+        result = value * MILES_TO_KM
+        self.root.ids.output_label.text = str(result)
 
 
 DistanceConverterApp().run()
