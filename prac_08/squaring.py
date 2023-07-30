19e94aa55ffd5d5df21a7a6fc80f0e30a6a26878
Hexon Hartley <hexonhartley.jimenez@my.jcu.edu.au>
2023-07-30 14:03:46 +1000

    Add estimated and actual time on header dosctring for squaring file

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -1,8 +1,8 @@
 """
 CP1404/CP5632 Practical 7 By Hexon Hartley Jimenez
 Kivy GUI program to square a number
-Estimated Time:  minutes
-Actual Time:  minutes
+Estimated Time: 30 minutes
+Actual Time: 28 minutes
 """
 
 from kivy.app import App
