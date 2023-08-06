450b00dae8578561dae72588abb4150000953449
Hexon Hartley <hexonhartley.jimenez@my.jcu.edu.au>
2023-08-06 14:19:53 +1000

    Add main function in file

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -7,10 +7,23 @@ Actual: 15 minutes
 # from prac_09 import taxi
 from taxi import Taxi
 
-# Create my_taxi object, with name, fuel and price
-my_taxi = Taxi("Prius 1", 100, 1.23)
-my_taxi.drive(40)
-print(f"{my_taxi}, current fare costs ${my_taxi.get_fare():.2f}")
-my_taxi.start_fare()
-my_taxi.drive(100)
-print(f"{my_taxi}, current fare costs ${my_taxi.get_fare():.2f}")
+
+def main():
+    # Create my_taxi object, with name, fuel and price
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+
+    # Drive the taxi at 40km
+    my_taxi.drive(40)
+
+    # Print taxi's details and current fare
+    print(f"{my_taxi}, current fare costs ${my_taxi.get_fare():.2f}")
+
+    # Start a new fare, then drive car at 100km
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+
+    # Print details of the new fare and cost
+    print(f"{my_taxi}, current fare costs ${my_taxi.get_fare():.2f}")
+
+
+main()
