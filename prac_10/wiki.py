2243200c07ca9d76250c2f545d665c94682a3a81
Hexon Hartley <hexonhartley.jimenez@my.jcu.edu.au>
2023-08-13 19:23:59 +1000

    Complete wiki program task

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -5,8 +5,17 @@ Wikipedia API & Python Library experimentation
 import wikipedia
 
 search_keyword = input("Enter a page title or search phrase: ")
-page = wikipedia.page(search_keyword)
-print(f"Searched Title: {page.title}")
-print(f"\nSummary{page.summary}")
-print(f"\n{page.url}")
+while search_keyword != "":
+    try:
+        page = wikipedia.page(search_keyword, auto_suggest=False)
+        print(f"Searched Title: {page.title}")
+        print(f"Summary:\n{wikipedia.summary(search_keyword, sentences=2)}")
+        print(f"\nURL: {page.url}")
+    except wikipedia.exceptions.DisambiguationError as e:
+        print("Disambiguation page found. Suggestions:", e.options)
+    except wikipedia.exceptions.PageError:
+        print("Page not found.")
 
+    search_keyword = input("\nEnter another page title or search phrase (blank to exit): ")
+print("Exiting...")
+print("Program Terminated")
